5d79b8b8aca6b0b2494fb9d63890174162fa7241
guidotorregiani <36246875+guidotorregiani@users.noreply.github.com>
2019-05-30 15:05:52 -0300

    Add files via upload

---
+++ PrimerParcialTerminado/main.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "orquesta.h"
+#include "musico.h"
+#include "instrumento.h"
+#include "utn.h"
+#include "informes.h"
+//#include <stdio_ext.h>
+
+
+int main()
+{
+    Orquesta arrayOrquesta[50];
+    Musico arrayMusico[1000];
+    Instrumento arrayInstrumento[20];
+
+    int aux;
+    int auxIdOrquesta;
+    int opcion;
+    int ready=0;
+    int idMusicoAnterior=0;
+    int idOrquestaAnterior=0;
+    int idInstrumentoAnterior=0;
+
+    orquesta_inicializar(arrayOrquesta,50);
+    musico_inicializar(arrayMusico,1000);
+    instrumento_inicializar(arrayInstrumento,20);
+
+      //hardcoded down here
+    arrayOrquesta[0].idOrquesta=1;
+    arrayOrquesta[1].idOrquesta=2;
+    arrayOrquesta[2].idOrquesta=3;
+    arrayOrquesta[3].idOrquesta=4;
+    strncpy(arrayOrquesta[0].nombre,"orquesta1",51);
+    strncpy(arrayOrquesta[1].nombre,"orquesta2",51);
+    strncpy(arrayOrquesta[2].nombre,"orquesta3",51);
+    strncpy(arrayOrquesta[3].nombre,"orquesta4",51);
+    strncpy(arrayOrquesta[0].lugar,"lugarA",51);
+    strncpy(arrayOrquesta[1].lugar,"lugarA",51);
+    strncpy(arrayOrquesta[2].lugar,"lugarB",51);
+    strncpy(arrayOrquesta[3].lugar,"lugarC",51);
+    arrayOrquesta[0].tipo=1;
+    arrayOrquesta[1].tipo=2;
+    arrayOrquesta[2].tipo=3;
+    arrayOrquesta[3].tipo=3;
+
+    //
+    arrayMusico[0].idMusico=1;
+    strncpy(arrayMusico[0].nombre,"mus1",51);
+    strncpy(arrayMusico[0].apellido,"Amus1",51);
+    arrayMusico[0].edad=30;
+    arrayMusico[0].idOrquesta=1;
+    arrayMusico[0].idInstrumento=2;
+    //
+    arrayMusico[1].idMusico=2;
+    strncpy(arrayMusico[1].nombre,"mus2",51);
+    strncpy(arrayMusico[1].apellido,"Amus2",51);
+    arrayMusico[1].edad=20;
+
+    arrayMusico[1].idOrquesta=2;
+    arrayMusico[1].idInstrumento=5;
+    //
+    arrayMusico[2].idMusico=3;
+    strncpy(arrayMusico[2].nombre,"mus3",51);
+    strncpy(arrayMusico[2].apellido,"Amus3",51);
+    arrayMusico[2].edad=25;
+    arrayMusico[2].idOrquesta=4;
+    arrayMusico[2].idInstrumento=2;
+    //
+    arrayMusico[3].idMusico=4;
+    strncpy(arrayMusico[3].nombre,"mus4",51);
+    strncpy(arrayMusico[3].apellido,"Amus4",51);
+    arrayMusico[3].edad=27;
+    arrayMusico[3].idOrquesta=4;
+    arrayMusico[3].idInstrumento=1;
+    //
+    arrayMusico[4].idMusico=5;
+    strncpy(arrayMusico[4].nombre,"mus5",51);
+    strncpy(arrayMusico[4].apellido,"Amus5",51);
+    arrayMusico[4].edad=22;
+    arrayMusico[4].idOrquesta=1;
+    arrayMusico[4].idInstrumento=3;
+    //
+    arrayMusico[5].idMusico=6;
+    strncpy(arrayMusico[5].nombre,"mus6",51);
+    strncpy(arrayMusico[5].apellido,"Amus6",51);
+    arrayMusico[5].edad=35;
+    arrayMusico[5].idOrquesta=3;
+    arrayMusico[5].idInstrumento=4;
+    //
+    arrayInstrumento[0].idInstrumento=1;
+    arrayInstrumento[1].idInstrumento=2;
+    arrayInstrumento[2].idInstrumento=3;
+    arrayInstrumento[3].idInstrumento=4;
+    arrayInstrumento[4].idInstrumento=5;
+    arrayInstrumento[0].tipo=1;
+    arrayInstrumento[1].tipo=2;
+    arrayInstrumento[2].tipo=2;
+    arrayInstrumento[3].tipo=3;
+    arrayInstrumento[4].tipo=4;
+    strncpy(arrayInstrumento[0].nombre,"inst1",51);
+    strncpy(arrayInstrumento[1].nombre,"inst2",51);
+    strncpy(arrayInstrumento[2].nombre,"inst3",51);
+    strncpy(arrayInstrumento[3].nombre,"inst4",51);
+    strncpy(arrayInstrumento[4].nombre,"inst5",51);
+
+    arrayOrquesta[0].isEmpty=0;
+    arrayOrquesta[1].isEmpty=0;
+    arrayOrquesta[2].isEmpty=0;
+    arrayOrquesta[3].isEmpty=0;
+    arrayInstrumento[0].isEmpty=0;
+    arrayInstrumento[1].isEmpty=0;
+    arrayInstrumento[2].isEmpty=0;
+    arrayInstrumento[3].isEmpty=0;
+    arrayInstrumento[4].isEmpty=0;
+    arrayMusico[0].isEmpty=0;
+    arrayMusico[1].isEmpty=0;
+    arrayMusico[2].isEmpty=0;
+    arrayMusico[3].isEmpty=0;
+    arrayMusico[4].isEmpty=0;
+    arrayMusico[5].isEmpty=0;
+
+
+    while(ready!=1)
+    {
+        fflush(stdin);
+        //__fpurge(stdin);
+        getValidInt(&opcion,"1:Alta Orquesta\n2:Baja Orquesta\n3:Modificar Orquesta\n4:Alta Musico\n5:Baja Musico\n6:Modificar Musico\n7:alta Instrumento\n8:Baja Instrumento\n9:A\n10:B\n11:C\n12:D\n13:E\n14:F\n15:G\n16:H\n17:salir\n","ERROR\n",1,17,3);
+
+        switch(opcion)
+        {
+            case 1:
+                orquesta_alta(arrayOrquesta,50,idOrquestaAnterior);
+                idOrquestaAnterior++;
+                printf("\nel id de esta orquesta es:%d\n",idOrquestaAnterior);
+                break;
+            case 2:
+                if(orquesta_baja(arrayOrquesta,50,&aux)==0)
+                {
+                    orquesta_bajaMusicos(arrayMusico,1000,aux);
+                }
+                break;
+            case 3:
+                orquesta_listar(arrayOrquesta,50);
+                orquesta_modificar(arrayOrquesta,50);
+                break;
+            case 4:
+                musico_alta(arrayMusico,1000,idMusicoAnterior);
+                idMusicoAnterior++;
+                printf("\nel id de este musico es:%d\n",idMusicoAnterior);
+                break;
+            case 5:
+                musico_baja(arrayMusico,1000);
+                break;
+            case 6:
+                musico_listar(arrayMusico,1000);
+                musico_modificar(arrayMusico,1000);
+                break;
+            case 7:
+                instrumento_alta(arrayInstrumento,20,idInstrumentoAnterior);
+                idInstrumentoAnterior++;
+                printf("\nel id de este instrumento es:%d\n",idInstrumentoAnterior);
+                break;
+            case 8:
+                instrumento_baja(arrayInstrumento,20);
+                break;
+            case 9:
+                informes_puntoA(arrayOrquesta,50,arrayMusico,1000);
+                break;
+            case 10:
+                informes_puntoB(arrayMusico,1000,
+                               arrayOrquesta,50,
+                               arrayInstrumento,20);
+                break;
+            case 11:
+                informes_puntoC(arrayOrquesta,50);
+                break;
+            case 12:
+                informes_puntoD(arrayOrquesta,50,arrayInstrumento,20,arrayMusico,1000);
+                break;
+            case 13:
+                fflush(stdin);
+                //__fpurge(stdin);
+                if(getValidInt(&auxIdOrquesta,"\nIngrese el id Orquesta\n","\nError\n",1,10000,2)==0)
+                {
+                    informes_puntoE(arrayMusico,1000,arrayInstrumento,20,auxIdOrquesta);
+                }
+                break;
+            case 14:
+                informes_contarCantidadMusicosPorOrquesta(arrayOrquesta,50,arrayMusico,1000);
+                informes_puntoF(arrayOrquesta,50);
+                break;
+            case 15:
+                informes_puntoG(arrayMusico,1000,arrayInstrumento,20,1);
+                break;
+            case 16:
+                informes_contarCantidadMusicosPorOrquesta(arrayOrquesta,50,arrayMusico,1000);
+                informes_puntoH(arrayOrquesta,50);
+                break;
+            case 17:
+                ready=1;
+                break;
+        }
+    }
+    return 0;
+}
---
+++ PrimerParcialTerminado/orquesta.c
@@ -0,0 +1,299 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+//#include <stdio_ext.h>
+#include "orquesta.h"
+#include "musico.h"
+#include "utn.h"
+#define Nombre_LEN 51
+
+/*void orquesta_menu(Orquesta *arrayOrquesta,int len,char* opcionesMenu, char* msgErr)
+{
+    int opcion;
+    int ready=0;
+    int id=0;
+
+    orquesta_inicializar(arrayOrquesta,1000);
+
+    while(ready!=1)
+    {
+        getValidInt(&opcion,opcionesMenu,msgErr,1,5,3);
+
+        switch(opcion)
+        {
+            case 1:
+                orquesta_alta(arrayOrquesta,1000,id);
+                id++;
+                break;
+            case 2:
+                orquesta_baja(arrayOrquesta,1000);
+                break;
+            case 3:
+                orquesta_modificar(arrayOrquesta,1000);
+                break;
+            case 4:
+                orquesta_promedioTipos(arrayOrquesta,1000);
+                orquesta_ordenarPorLugar(arrayOrquesta,1000,1);
+                orquesta_listar(arrayOrquesta,1000);
+                break;
+            case 5:
+                ready=1;
+                break;
+        }
+    }
+}*/
+
+int orquesta_inicializar(Orquesta *arrayOrquesta,int len)
+{
+    int i;
+    int ret=-1;
+    if(arrayOrquesta != NULL && len >= 0)
+    {
+        for(i=0;i<len;i++)
+        {
+            arrayOrquesta[i].isEmpty=1;
+            arrayOrquesta[i].cantMusicos=0;
+            ret=0;
+        }
+    }
+    return ret;
+}
+int orquesta_lugarLibre(Orquesta *arrayOrquesta,int len,int *lugarLibre)
+{
+    int i;
+    int ret=-1;
+    if(arrayOrquesta != NULL && len >= 0)
+    {
+        for(i=0; i<len ; i++)
+        {
+            if(arrayOrquesta[i].isEmpty==1)
+            {
+                *lugarLibre = i;
+                ret=0;
+                break;
+            }
+        }
+    }
+    return ret;
+}
+
+int orquesta_alta(Orquesta *arrayOrquesta,int len,int idOrquestaAnterior)
+{
+    int ret=-1;
+    int flag=1; // 0 falso(dio mal) 1 verdadero(dio bien)
+    int lugarLibre;
+    int auxTipo;
+    char auxNombre[Nombre_LEN];
+    char auxLugar[Nombre_LEN];
+
+
+    if(arrayOrquesta != NULL && len >= 0)
+    {
+        if(orquesta_lugarLibre(arrayOrquesta,50,&lugarLibre)==0)
+        {
+            if(getString(auxNombre,"Ingrese un nombre: \n", "ERROR!\n", 1, Nombre_LEN, 2)!=0)
+            {
+                flag=0;
+            }
+            if(getString(auxLugar,"Ingrese lugar: \n", "ERROR!\n", 1, Nombre_LEN, 2)!=0)
+            {
+                flag=0;
+            }
+            if(getValidInt(&auxTipo,"Ingrese el tipo\n","ERROR!\n",1,3,2)!=0)
+            {
+                flag=0;
+            }
+
+            if(flag==1)
+            {
+                strncpy(arrayOrquesta[lugarLibre].nombre,auxNombre,Nombre_LEN);
+                strncpy(arrayOrquesta[lugarLibre].lugar,auxLugar,Nombre_LEN);
+                arrayOrquesta[lugarLibre].tipo=auxTipo;
+                arrayOrquesta[lugarLibre].isEmpty=0;
+                arrayOrquesta[lugarLibre].idOrquesta=idOrquestaAnterior+1;
+                ret=0;
+            }
+            else
+            {
+                printf("error, campo mal ingresado, el orquesta no se guardara");
+                arrayOrquesta[lugarLibre].isEmpty=1;
+            }
+
+            /*Debuggers
+            printf(" %s\n %s\n %d\n %d\n",arrayOrquesta[lugarLibre].lugar
+                                         ,arrayOrquesta[lugarLibre].nombre
+                                         ,arrayOrquesta[lugarLibre].sector
+                                         ,arrayOrquesta[lugarLibre].tipo);
+            printf(" %d\n",lugarLibre);
+            printf("%d\n",arrayOrquesta[lugarLibre].isEmpty);
+            printf("%d\n",arrayOrquesta[lugarLibre].idOrquesta);*/
+        }
+
+    }
+    return ret;
+}
+
+int orquesta_buscarID(Orquesta *arrayOrquesta, int size, int id, int* posicion)
+{
+    int ret=-1;
+    int i;
+    if(arrayOrquesta!= NULL && size>=0)
+    {
+        for(i=0;i<size;i++)
+        {
+            if(arrayOrquesta[i].isEmpty==1)
+            {
+                continue;
+            }
+            else if(arrayOrquesta[i].idOrquesta==id)
+            {
+                ret=0;
+                *posicion=i;
+                break;
+            }
+        }
+    }
+    return ret;
+}
+
+
+int orquesta_modificar(Orquesta *arrayOrquesta,int len)
+{
+    int ret=-1;
+    int idOrquesta;
+    int posicionDeLaCoincidencia=0;
+    int ready=0;
+    int opcion;
+
+    if(arrayOrquesta != NULL && len >= 0)
+    {
+        getValidInt(&idOrquesta,"Ingrese el id del orquesta a modificar\n","error\n",1,50,3);
+        if(orquesta_buscarID(arrayOrquesta,1000,idOrquesta,&posicionDeLaCoincidencia)!=0)
+        {
+            printf("Error, el id no existe o el array es nulo\n");
+        }
+        else
+        {
+            while(ready!=1)
+            {
+                fflush(stdin);
+                //__fpurge(stdin);
+                getValidInt(&opcion,"Ingrese valor a modificar:\n 1 para nombre\n2 para lugar\n3para tipo\4 para salir","Error",1,4,3);
+
+                switch(opcion)
+                {
+                    case 1:
+                        getString(arrayOrquesta[posicionDeLaCoincidencia].nombre,"\nNuevo valor para nombre:\n","\nError\n",1,Nombre_LEN,2);
+                        break;
+                    case 2:
+                        getString(arrayOrquesta[posicionDeLaCoincidencia].lugar,"\nNuevo valor para lugar:\n","\nError\n",1,Nombre_LEN,2);
+                        break;
+                    case 3:
+                        getValidInt(&arrayOrquesta[posicionDeLaCoincidencia].tipo,"\nIngrese el nuevo valor para tipo\n","\nError\n",1,3,2);
+                        break;
+                    case 4:
+                        ready=1;
+                        break;
+                }
+            }
+        }
+    }return ret;
+}
+
+int orquesta_baja(Orquesta *arrayOrquesta,int len,int* auxId)
+{
+
+    int ret=-1;
+    int idOrquesta;
+    int posicionDeLaCoincidencia=0;
+
+    if(arrayOrquesta != NULL && len >= 0)
+    {
+        getValidInt(&idOrquesta,"\nIngrese el id del orquesta a dar de baja\n","\nerror\n",1,9999,3);
+        if(orquesta_buscarID(arrayOrquesta,30,idOrquesta,&posicionDeLaCoincidencia)==0)
+        {
+            arrayOrquesta[posicionDeLaCoincidencia].isEmpty=1;
+            printf("se ha dado de baja el orquesta en la posicion %d\n",posicionDeLaCoincidencia);
+            *auxId=idOrquesta;
+            ret=0;
+        }
+        else
+        {
+            printf("Error, el id no existe o el array es nulo");
+        }
+    }
+    return ret;
+}
+int orquesta_bajaMusicos(Musico *arrayMusico, int len, int valorBuscado)
+{
+    int retorno=-1;
+    int i;
+    if(arrayMusico!=NULL && len>0)
+    {
+        for(i=0;i<len;i++)
+        {
+            if(arrayMusico[i].idOrquesta==valorBuscado)                                                        //cambiar si no se busca por ID
+            {
+                arrayMusico[i].isEmpty=1;
+            }
+        }
+        retorno=0;
+    }
+    return retorno;
+}
+
+
+int orquesta_listar(Orquesta *arrayOrquesta,int len)
+{
+    int i;
+    int ret=-1;
+    if(arrayOrquesta!=NULL && len>=0)
+    {
+        printf("\nOrquestas");
+        for(i=0;i<len;i++)
+        {
+            if(arrayOrquesta[i].isEmpty==0)
+            {
+                    printf(" %s\n %s\n %d\n %d\n",arrayOrquesta[i].lugar
+                                                 ,arrayOrquesta[i].nombre
+                                                 ,arrayOrquesta[i].tipo
+                                                 ,arrayOrquesta[i].idOrquesta);
+            }
+        }
+    }
+    return ret;
+}
+
+/*int orquesta_sortOrquestaCantMusicos(Orquesta *arrayOrquesta,int lenOrquesta)
+{
+    int i;
+    int j;
+    Orquesta buffer;
+    int ret = -1;
+
+    if(arrayOrquesta != NULL && len > 0)
+    {
+        for(i=0;i<len-1;i++)
+        {
+            for(j=i+1;j<len;j++)
+            {
+                if((strcmp(arrayOrquesta[j].name,arrayOrquesta[i].name) < 0))
+                {
+                    buffer = arrayOrquesta[i];
+                    arrayOrquesta[i] = arrayOrquesta[j];
+                    arrayOrquesta[j] = buffer;
+                    ret = 0;
+                }
+                else if((strcmp(arrayOrquesta[j].name,arrayOrquesta[i].name) > 0))
+                {
+                    buffer = arrayOrquesta[i];
+                    arrayOrquesta[i] = arrayOrquesta[j];
+                    arrayOrquesta[j] = buffer;
+                    ret = 0;
+                }
+            }
+        }
+    }
+    return ret;
+}*/
